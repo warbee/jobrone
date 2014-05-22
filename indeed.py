62b3a15340b1b740d4f3325773b1bf75c84f6d80
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-21 23:02:44 -0700

    adding indeed api call

---
+++ indeed.py
@@ -0,0 +1,60 @@
+import urllib as u
+import urllib2 as u2
+from lxml import etree
+
+publisher_id = 7837020139926262
+query = {}
+indeed_url = 'http://api.indeed.com/ads/apisearch?'
+startVal = 0
+
+def buildQuery(job):
+	query['q'] = job
+	query['l'] = 'san diego, ca'
+	query['v'] = 2
+	query['publisher'] = publisher_id
+	query['start'] = startVal
+
+	query_string = u.urlencode(query)
+
+	return indeed_url + query_string
+
+
+def getResponse(query):
+	req = u2.Request(query)
+	response = u2.urlopen(req)
+	data = response.read()
+	return data
+
+
+def parseXML(data):
+	root = etree.fromstring(data)
+	#for element in root.iter():
+	#	print "%s - %s" % (element.tag, element.text)
+	#use jobkey for community  page
+		
+	return root
+
+#data.xpath('/response/totalresults')  #list type
+#titles = data.xpath('/response/results/result/jobtitle')
+def getTree(job):
+	q = buildQuery(job)
+	resp = getResponse(q)
+	data = parseXML(resp)
+	return data
+
+
+#--------------------------------------------------------------------
+#
+#				Parsing functions to use on the site.
+#
+#--------------------------------------------------------------------
+def getTotalResults(data):
+	total = data.xpath('/response/totalresults')
+	num = total[0].text
+	return num
+
+
+#Easier to return all results
+def getResults(data):
+	results = data.xpath('/response/results/result')
+	return results
