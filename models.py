d1a80858a3147b12634cb5b2d7f7ca8cea6c2963
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-21 23:08:17 -0700

    added jobs table.

--- models.py
+++ models.py
@@ -9,9 +9,31 @@ class User(db.Model):
     created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 
-    def __init__(self, email):
+    def __init__(self, email, created):
         self.email = email
+        self.created = created
 
     def __repr__(self):
         return '<id %r>' % self.id
 
+
+class Jobs(db.Model):
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique = True)
+    first_job = db.Column(db.String(1000))
+    second_job = db.Column(db.String(1000))
+    third_job = db.Column(db.String(1000))
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+    def __init__(self, user_id, first_job, second_job, third_job, created):
+        self.user_id = user_id
+        self.first_job = first_job
+        self.second_job = second_job
+        self.third_job = third_job
+        self.created = created
+
+    def __repr__(self):
+        return '<id %r>' % self.id
