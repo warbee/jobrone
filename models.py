0320a9c010f295dc5c0ba6be0c971cddc83a7f8f
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-21 22:40:47 -0700

    updated db model to have user email and created date.

--- models.py
+++ models.py
@@ -1,30 +1,16 @@
 from app import db
-
-class Result(db.Model):
-    __tablename__ = 'results'
-
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(255))
-    result_all = db.Column(db.String(4000))
-    result_no_stop_words = db.Column(db.String(4000))
-
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
-
-    def __repr__(self):
-        return '<id %r>' % self.id
-
+import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    email = db.Column(db.String(1000))
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-    def __init__(self, name):
-        self.name = name
+
+    def __init__(self, email):
+        self.email = email
 
     def __repr__(self):
         return '<id %r>' % self.id
