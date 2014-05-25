2077e6392766b3c52720883fcd2af463585c0db6
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-25 10:23:24 -0700

    Added simple form in jumbotron.
    Added indeed library, and added initial GET request for the first form.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, url_for, render_template, flash
+from flask import Flask, url_for, render_template, flash, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import indeed
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -12,6 +13,10 @@ import models
 def index():
     return render_template('index.html') 
 
+@app.route('/start', methods=['GET'])
+def getStarted():
+	first_search = request.args.get('first_search')
+	return render_template('right-sidebar.html', first_search=first_search)
 
 if __name__ == '__main__':
     app.run()
