a85676d4d30b374864b4d8cb04bff7faabf4d2ce
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-25 10:33:15 -0700

    Updated db model
    Updated model to have selected jobs and ignored jobs.

--- models.py
+++ models.py
@@ -17,8 +17,8 @@ class User(db.Model):
         return '<id %r>' % self.id
 
 
-class Jobs(db.Model):
-    __tablename__ = 'jobs'
+class Chosen(db.Model):
+    __tablename__ = 'chosen_jobs'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique = True)
@@ -33,6 +33,24 @@ class Jobs(db.Model):
         self.first_job = first_job
         self.second_job = second_job
         self.third_job = third_job
+        self.created = created
+
+    def __repr__(self):
+        return '<id %r>' % self.id
+
+
+class Ignored(db.Model):
+    __tablename__ = 'ignored_jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique = True)
+    job = db.Column(db.String(1000))
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+    def __init__(self, user_id, job, created):
+        self.user_id = user_id
+        self.job = job
         self.created = created
 
     def __repr__(self):
