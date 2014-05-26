663fa9f9f5f0e0a62a486752a5be5bde522a277d
Warren Bartolome <warren.bartolome@gmail.com>
2014-05-25 19:43:58 -0700

    Added counter to try and re-use wizard route.

--- app.py
+++ app.py
@@ -13,36 +13,39 @@ import models
 def index():
     return render_template('index.html') 
 
-@app.route('/start', methods=['GET'])
+@app.route('/wizard', methods=['GET'])
 def getStarted():
+	screen = request.args.get('screen')
 	email = request.args.get('email')
 	first_search = request.args.get('first_search')
 	
 	data = indeed.getTree(first_search)
 	results = indeed.getResults(data)
 
-	global skill
-	global user
-	user = models.User
-	skill = models.Skills
-	db = models.db
+	if screen == 1:
+		global skill
+		global user
+		user = models.User
+		skill = models.Skills
+		db = models.db
 
-	global visitor
-	try:
-		visitor = user(email=email, created=None)
-		db.session.add(visitor)
+		global visitor
+		try:
+			visitor = user(email=email, created=None)
+			db.session.add(visitor)
+			db.session.commit()
+		except Exception as e:
+			db.session.rollback()
+			visitor = user.query.filter_by(email=email).first()
+
+		first_skill = skill(user_id=visitor.id, skill=first_search, created=None)
+		db.session.add(first_skill)
 		db.session.commit()
-	except Exception as e:
-		visitor = user.query.filter_by(email=email).first()
-
-	first_skill = skill(user_id=visitor.id, skill=first_search, created=None)
-	db.session.add(first_skill)
-	db.session.commit()
 	
 	return render_template('joblist.html', first_search=first_search,
 										   results=results,
 										   email=email,
-										   skill_check=skill.id,
+										   skill_check=first_skill.id,
 										   visitor_id=visitor.id)
 
 if __name__ == '__main__':
